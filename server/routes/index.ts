7ca4e6aca3296ac2969b42c09cf495e056c7d7c7
Andrei Serban <andrei.serban@brewingbytes.com>
2023-11-26 00:49:15 +0200

    create server side search

--- server/routes/index.ts
+++ server/routes/index.ts
@@ -1,9 +1,11 @@
 import express, { Express } from 'express';
 import version from './version';
+import search from './search';
 
 const router: Express = express();
 
 router.use('/version', version);
+router.use('/search', search);
 router.use('*', (req, res) => {
     res.status(404).json({ err: 'Not Found' });
 });
