087eac2c8a2ffd45f2023e8da4ab512036b3fbbb
Serban Andrei <andrei.serban2409@proton.me>
2023-11-25 22:09:11 +0200

    add bottom navbar for volume and song controls (#7)

--- ui/src/layouts/default/Default.vue
+++ ui/src/layouts/default/Default.vue
@@ -1,12 +1,13 @@
 <template>
     <v-app>
         <default-drawer />
-
+        <default-bottom-nav />
         <default-view />
     </v-app>
 </template>
 
 <script lang="ts" setup>
 import DefaultDrawer from './AppDrawer.vue';
+import DefaultBottomNav from './AppBottomNav.vue';
 import DefaultView from './View.vue';
 </script>
