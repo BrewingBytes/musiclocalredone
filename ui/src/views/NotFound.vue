e07f026571a1f97157821efd18a05580ac384595
Serban Andrei <andrei.serban2409@proton.me>
2023-11-25 19:27:45 +0200

    Created App Drawer (#5)

--- ui/src/views/NotFound.vue
+++ ui/src/views/NotFound.vue
@@ -2,10 +2,8 @@
     <v-container>
         <v-row>
             <v-col cols="12">
-                <h1>Home</h1>
+                <h1>Not Found</h1>
             </v-col>
         </v-row>
     </v-container>
 </template>
-
-<script lang="ts" setup></script>
