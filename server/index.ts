e01b7302cc517641ca78d1434e867fa6096fb322
Andrei Serban <andrei.serban@brewingbytes.com>
2023-11-25 18:25:32 +0200

    add prettier and download

--- server/index.ts
+++ server/index.ts
@@ -1,16 +1,16 @@
 import express, { Express } from 'express';
 import dotenv from 'dotenv';
-import cors from "cors";
+import cors from 'cors';
 
 import api from './routes';
 
 const allowedOrigins = [
-    "http://localhost:3000",
-    "http://localhost:5001",
-    "http://localhost:80",
-    "http://localhost",
-    "http://music.local",
-    "http://music.local:80",
+    'http://localhost:3000',
+    'http://localhost:5001',
+    'http://localhost:80',
+    'http://localhost',
+    'http://music.local',
+    'http://music.local:80'
 ];
 
 function main() {
@@ -22,14 +22,18 @@ function main() {
     app.use(express.json());
     app.use(express.urlencoded({ extended: true }));
     app.use(express.static('public'));
-    app.use(cors({
-        origin: allowedOrigins,
-        credentials: false
-    }));
+    app.use(
+        cors({
+            origin: allowedOrigins,
+            credentials: false
+        })
+    );
     app.use(api);
 
     app.listen(port, () => {
-        console.log(`⚡️[server]: Server is running at http://localhost:${port}`);
+        console.log(
+            `⚡️[server]: Server is running at http://localhost:${port}`
+        );
     });
 }
 
