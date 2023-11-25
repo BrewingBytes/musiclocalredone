17598c94931a1fa91a59d8c53c7895862258906f
Andrei Serban <andrei.serban@brewingbytes.com>
2023-11-26 01:11:49 +0200

    create client side search

--- server/routes/search.ts
+++ server/routes/search.ts
@@ -1,5 +1,4 @@
 import express, { Express, Request, Response } from 'express';
-import ytdl from 'ytdl-core';
 import YouTube = require('youtube-node');
 
 const router: Express = express();
@@ -13,10 +12,13 @@ interface ISearchQuery extends Request {
 router.post('/query', (req: ISearchQuery, res: Response) => {
     const { query } = req.body;
 
-    const yt = new YouTube()
+    const yt = new YouTube();
 
     yt.setKey(process.env.YOUTUBE_API_KEY || '');
+    yt.addParam('type', 'video');
+    
 
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any
     yt.search(query, 10, (err: any, result: any) => {
         if (err) {
             console.log(err);
@@ -25,7 +27,6 @@ router.post('/query', (req: ISearchQuery, res: Response) => {
             res.status(200).json({ err: 'NO_ERROR', payload: result.items });
         }
     });
-    
 });
 
 export default router;
